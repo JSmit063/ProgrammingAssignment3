e1945b1dab217270747602c43bdf58c242bd230c
Justin Smith <smithj359@macomb.edu>
2022-06-08 22:34:39 -0400

    Started programming assignment 3, includes variable declarations and header. No control structures.

--- ProgrammingAssignment3.cpp
+++ ProgrammingAssignment3.cpp
@@ -1,10 +1,39 @@
 // Programming Assignment 3
 // Justin Smith
 
-#include <iostream>
-#include 
+#include <iostream> // This program uses I/O streams
+#include <string> // Using string objects
+#include <fstream> // Need to output data to external txt file
+#include <iomanip> // Needed to use format manipulators
+
+using namespace std;
+
+const double FRAGILE = 2.00; // Constants
+
+// Program resides entirely within main function
 int main()
 {
-    
+    // Variable declarations
+    string itemName;
+    string destinationName;
+    double orderTotal;
+    double shippingCost;
+    double totalWithShipping;
+
+    cout << "*******************************************************************************" << endl;
+    cout << "******************** ITCS 2530 -- Programming Assignment 3 ********************" << endl;
+    cout << "*******************************************************************************" << endl << endl;
+
+    // Need to gather input from the user to find the name, fragility of the item, order total, and destination
+
+
+    // We need to find the shipping cost accounting for the destination
+    // There are 4 tiers of shipping costs and each tier is a different amount for each destination
+    // The following code uses selection structures (if/else) to resolve the shipping cost and new total
+
+
+    system("pause");
+
+    return 0;
 }
 
